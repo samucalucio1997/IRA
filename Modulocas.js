1791787a98764879b2c5f8e2592aeb71179e77be
Samuel Lúcio Moreira de Farias <20222014040004@ifrn.local>
2024-03-15 12:44:18 -0300

    sprint final

--- Modulocas.js
+++ Modulocas.js
@@ -6,7 +6,7 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-
+app.use(express.static('./'))
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
--- recuperarClientes.js
+++ recuperarClientes.js
@@ -1,4 +1,4 @@
-url = 'https://3.128.249.166:3000/clients/'
+url = 'http://3.128.249.166:8000/api/clients/'
 
 export function eClientes() {
     return fetch(url);
