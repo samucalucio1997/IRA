5208df4d25f065b53650243bac2af39ffc13afd4
Samuel Lúcio <samucalucio@hotmail.com>
2024-03-22 22:05:30 -0300

    consumo api gatos

--- funcoes.js
+++ funcoes.js
@@ -1,16 +1,19 @@
-import { eClientes } from './recuperarClientes';
 
-var id = document.getElementById('id');
-var nome = document.getElementById('nome');
-var email = document.getElementById('email');
-console.log(eClientes);
 
-eClientes
-.then( e=> {
-    e.map(el => {
-        id.innerHTML = el['id'];
-        nome.innerHTML = el['nome'];
-        email.innerHTML = el['email'];
-    })
-    console.log(e);
-})
+var id = document.getElementById('prin');
+// console.log(eClientes);
+function api(){
+    fetch('https://api.thecatapi.com/v1/images/search?limit=10').then(
+        n => v = n.json())
+        .then(data => {
+            data.map(el => {
+                id.innerHTML += `
+                  <td>${el.id}</td>
+                  <td> <img src="${el.url}" width="${el.width}" height="${el.height}"/> </td>
+                 `
+                // console.log(el['width'])
+            })
+        }
+            ).catch(e => console.log(e));
+}
+api();
--- recuperarClientes.js
+++
@@ -1,5 +0,0 @@
-url = 'http://3.128.249.166:8000/api/clients/'
-
-export function eClientes() {
-    return fetch(url);
-}
